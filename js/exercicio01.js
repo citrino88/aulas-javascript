fbd2f1136cb313c6d7d464b348c139b506743350
citrino88 <almeida.citrino@gmail.com>
2024-04-18 14:59:55 -0300

    exercicio de condicional sucessiva

---
+++ js/exercicio01.js
@@ -0,0 +1,19 @@
+`use strict`
+
+console.log("Exercicio")
+/* let global e bloco */
+let nota1 = 9;
+let nota2 = 6;
+
+// aqui media pode ser const ou let tbm, const pq ela não esta alterando a função
+const media = (nota1 + nota2) / 2;
+
+console.log(media);
+
+if (media >= 7){
+    console.log("APROVADO"); 
+}else if (media >= 5){
+console.log("RECUPERAÇÃO"); 
+}else{
+console.log("REPROVADO");    
+}
