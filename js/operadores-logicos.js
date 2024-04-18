7c77327041e08532670b4234244545f4b816890e
citrino88 <almeida.citrino@gmail.com>
2024-04-18 16:03:14 -0300

    operadores logicos JS ou negação

--- js/operadores-logicos.js
+++ js/operadores-logicos.js
@@ -21,3 +21,35 @@ if( mediaFinal  >= mediaMinima && faltas <= limiteDeFaltas ){
 }
 
 console.log(resultado);
+
+console.log("--------------------");
+
+//No caso abaixo OU uma das condições não precisa necessariamente ser verdadeira.
+// Operador OU/ OR (||) Obs: O símbolo | é chamado PIPE
+let idade = 25;
+
+if ( idade >= 18 || idade < 12){
+    console.log("Você é um adulto ou criança!");
+}else {
+    console.log("Você é um adolescente");
+}
+
+let onibus = 2203;//2203 OU 2019
+
+if (onibus == 2203 || onibus==2019){
+    console.log("Passa no Senac, embarque!");
+}else{
+    console.log("Não passa no Senac, aguarde!");
+}
+
+console.log("-----------------");
+
+//Operador NEGAÇÃO/NOT (!)
+let blackFriday = false; // false simboliza "não vigorando"
+
+//Se blackFriday NÃO FOR verdadeiro
+if ( !blackFriday ){
+    console.log("Preços normais...");
+} else {
+    console.log("Black Friday vigorando! Descontos!");
+}
