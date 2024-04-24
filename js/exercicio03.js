ef4f7b40c24d282797ad30fb9aa0125c70152777
citrino88 <almeida.citrino@gmail.com>
2024-04-24 16:02:55 -0300

    antes de trocar localizacao pt-br

--- js/exercicio03.js
+++ js/exercicio03.js
@@ -3,11 +3,53 @@
 // Dados de entrada
 let cotacaoDoDia = 5.16;
 let valorEmDolar = 1000;
-let conversao = cotacao * valorEmDolar
+//let valorEmReal = 5160;
+
+//let conversaoDolarEmReal = cotacaoDoDia * valorEmDolar;
+//let conversaoRealEmDolar = valorEmReal / cotacaoDoDia;
+
+//console.log(conversaoDolarEmReal);
+//console.log(conversaoRealEmDolar);
 
 /* 1) Crie uma funÃ§Ã£o para converter */
+
+function converterEmReais(valor){
+    return valor * cotacaoDoDia;
+
+}
 
 /* 2.1) Chame a funÃ§Ã£o e passe pra ela o valorEmDolar como
 parÃ¢metro */
 
+//Versão sem formatação 
+console.log( converterEmReais (valorEmDolar));
+
 /* 2.2) Mostre no console o retorno da funÃ§Ã£o */
+
+console.log(`-----------------------------`);
+
+function formatarMoeda(valor, siglaDaMoeda, localizacao){
+    const opcoes = {
+        style: "currency",
+        currency: siglaDaMoeda
+    };
+
+    return new Intl.NumberFormat(
+        "pt-br", opcoes
+    ).format(valor);
+}
+
+// Versão sem formatação
+console.log( converterEmReais(valorEmDolar) );
+
+// Versão 1 (função como parâmetro para outra função) com formatação
+console.log( 
+    formatarMoeda( converterEmReais(valorEmDolar), "BRL" ) 
+);
+
+// Versão 2 (primeiro, guardamos o resultado da conversão; depois passamos o resultado para a formatação)
+let resultadoEmReais = converterEmReais(valorEmDolar);
+console.log( formatarMoeda(resultadoEmReais , "BRL") );
+
+//Formatação valor em dólar
+console.log( formatarMoeda(valorEmDolar , "USD") );
