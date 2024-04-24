8a30962f364997e1496cb3d77fc2ba3bc2afc302
citrino88 <almeida.citrino@gmail.com>
2024-04-24 16:33:39 -0300

    estudo DOM js

---
+++ 08-dom.js
@@ -0,0 +1,14 @@
+`use strict`;
+
+/* Funções/métodos mais comuns para seleção de elementos no DOM
+
+- getElementById()
+Selecionar UM ELEMENTO através do atributo ID
+
+- querySelector()
+Seleciona UM ELEMENTO através de seletor baseado na lógica CSS
+
+- querySelectorAll()
+Seleciona VÁRIOS ELEMENTOS através de seletor(es) baseado na lógica CSS
+
+*/
