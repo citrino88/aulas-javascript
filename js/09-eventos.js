682812e5886b0b654a52ab201c17ea85355f5acf
citrino88 <almeida.citrino@gmail.com>
2024-04-25 15:28:46 -0300

    evento click e com audio JS

--- js/09-eventos.js
+++ js/09-eventos.js
@@ -2,6 +2,28 @@
 
 // Seleção dos elementos
 const exemplo01 = document.querySelector("#exemplo01");
+const mensagem = document.querySelector("#mensagem");
+const pagina = document.querySelector("body");
 
-// Criando um Ouvinte de Evento (Event Listener)
-exemplo01.addEventListener("");
+// Criando um objeto de áudio referenciando o arquivo
+const efeitoSonoro = new Audio();
+efeitoSonoro.src = "wow.wav";
+
+/* Criando um Ouvinte de Evento (Event Listener)
+O primeiro parâmetro do addEvenListener é o tipo de evento que queremos "ouvir/monitorar". O segundo parâmetro é uma função (conhecida como callBack "uma chamada de função") responsável por executar ações no momento em que o evento acontecer.
+*/ 
+// prestar atenção quando escrever o nome do evento, se escrever errado o VSC, não avisa nem aparece erro no console.
+exemplo01.addEventListener("click", function(){
+   // alert("Oieeeeeeee");
+   // Exibindo um texto no parágrafo mensagem
+    mensagem.textContent = "Beleza! Evento Acionado, função executada 😍";
+
+    // Ativando uma classe para o mensagem
+    mensagem.classList.add("destaque");
+
+    // Modificando a fonte do body
+    pagina.style.fontFamily = "Garamond";
+
+    // Executando o áudio
+    efeitoSonoro.play();
+});
