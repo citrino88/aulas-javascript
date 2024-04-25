717df3413bb09051d2c3a114dc6ae6413336ea86
citrino88 <almeida.citrino@gmail.com>
2024-04-25 14:56:09 -0300

    09 eventos JS

---
+++ js/09-eventos.js
@@ -0,0 +1,7 @@
+`use strict`
+
+// Seleção dos elementos
+const exemplo01 = document.querySelector("#exemplo01");
+
+// Criando um Ouvinte de Evento (Event Listener)
+exemplo01.addEventListener("");
